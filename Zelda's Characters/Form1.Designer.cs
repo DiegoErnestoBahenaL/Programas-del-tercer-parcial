1b81c190cc5a0aeda9bfa70682b814d39abd851b
Diego Ernesto Bahena López <dbahenalopez@hotmail.com>
2019-05-19 13:26:01 -0500

    Program is able to search images, save them in combobox and listbox and delete them from list bos showing a message

--- Zelda's Characters/Form1.Designer.cs
+++ Zelda's Characters/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.Enter_character = new System.Windows.Forms.TextBox();
-            this.serch_character = new System.Windows.Forms.TextBox();
+            this.search_character = new System.Windows.Forms.TextBox();
             this.characters_names = new System.Windows.Forms.ListBox();
             this.Character_picture = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.Character_picture)).BeginInit();
@@ -39,41 +39,44 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(263, 64);
+            this.comboBox1.Location = new System.Drawing.Point(641, 150);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(266, 21);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Seleccione un personaje";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Enter_character
             // 
-            this.Enter_character.Location = new System.Drawing.Point(263, 14);
+            this.Enter_character.Location = new System.Drawing.Point(641, 106);
             this.Enter_character.Name = "Enter_character";
             this.Enter_character.Size = new System.Drawing.Size(266, 20);
             this.Enter_character.TabIndex = 1;
             this.Enter_character.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.Enter_character.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
-            // serch_character
+            // search_character
             // 
-            this.serch_character.Location = new System.Drawing.Point(263, 638);
-            this.serch_character.Name = "serch_character";
-            this.serch_character.Size = new System.Drawing.Size(266, 20);
-            this.serch_character.TabIndex = 2;
-            this.serch_character.TextChanged += new System.EventHandler(this.serch_character_TextChanged);
+            this.search_character.Location = new System.Drawing.Point(268, 584);
+            this.search_character.Name = "search_character";
+            this.search_character.Size = new System.Drawing.Size(266, 20);
+            this.search_character.TabIndex = 2;
+            this.search_character.TextChanged += new System.EventHandler(this.serch_character_TextChanged);
+            this.search_character.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.serch_character_KeyPress);
             // 
             // characters_names
             // 
             this.characters_names.FormattingEnabled = true;
-            this.characters_names.Location = new System.Drawing.Point(27, 31);
+            this.characters_names.Location = new System.Drawing.Point(36, 87);
             this.characters_names.Name = "characters_names";
             this.characters_names.Size = new System.Drawing.Size(120, 459);
             this.characters_names.TabIndex = 3;
             this.characters_names.SelectedIndexChanged += new System.EventHandler(this.characters_names_SelectedIndexChanged);
+            this.characters_names.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.characters_names_KeyPress);
             // 
             // Character_picture
             // 
-            this.Character_picture.Location = new System.Drawing.Point(250, 118);
+            this.Character_picture.Location = new System.Drawing.Point(248, 210);
             this.Character_picture.Name = "Character_picture";
             this.Character_picture.Size = new System.Drawing.Size(304, 326);
             this.Character_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -85,14 +88,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 683);
+            this.ClientSize = new System.Drawing.Size(931, 683);
             this.Controls.Add(this.Character_picture);
             this.Controls.Add(this.characters_names);
-            this.Controls.Add(this.serch_character);
+            this.Controls.Add(this.search_character);
             this.Controls.Add(this.Enter_character);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Character_picture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,7 +107,7 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox Enter_character;
-        private System.Windows.Forms.TextBox serch_character;
+        private System.Windows.Forms.TextBox search_character;
         private System.Windows.Forms.ListBox characters_names;
         private System.Windows.Forms.PictureBox Character_picture;
     }
--- Zelda's Characters/Form1.cs
+++ Zelda's Characters/Form1.cs
@@ -26,34 +26,64 @@ namespace WindowsFormsApplication1
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if ( e.KeyChar == Convert.ToChar(Keys.Enter))
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
             {
                 comboBox1.Items.Add(Enter_character.Text); 
                 characters_names.Items.Add(Enter_character.Text);
                 Character_picture.Load(@"..\..\Images\" + Enter_character.Text + ".jpg");
                 Enter_character.Text = ("");
+                
                
             }
         }
 
         private void serch_character_TextChanged(object sender, EventArgs e)
         {
-
+              
         }
 
         private void characters_names_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+           
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Character_picture.Load(@"..\..\Images\" + comboBox1.Text + ".jpg");
         }
 
         private void Character_picture_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serch_character_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar (Keys.Enter))
+            {
+                Character_picture.Load(@"..\..\Images\" + search_character.Text + ".jpg");
+            }
+
+
+        }
+
+        private void characters_names_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+            {
+                DialogResult Answer = MessageBox.Show("El personaje " + characters_names.Text + " será eliminado, ¿Está seguro?", "Advertencia", MessageBoxButtons.YesNo);
+
+                if (Answer == DialogResult.Yes)
+                {
+                    characters_names.Items.RemoveAt(characters_names.SelectedIndex);
+                }
+
+            }
         }
     }
 }
