f4cf8f657c84564f27fdab88b586d4e3870813d8
Diego Ernesto Bahena López <dbahenalopez@hotmail.com>
2019-05-19 20:12:58 -0500

    Program shows a progress bar before any action of the textboxes or the combobox; also shows a description of the image from a .txt file

--- Zelda's Characters/Form1.Designer.cs
+++ Zelda's Characters/Form1.Designer.cs
@@ -28,38 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.Enter_character = new System.Windows.Forms.TextBox();
             this.search_character = new System.Windows.Forms.TextBox();
             this.characters_names = new System.Windows.Forms.ListBox();
             this.Character_picture = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.Character_picture)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(641, 150);
+            this.comboBox1.Location = new System.Drawing.Point(629, 153);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(266, 21);
+            this.comboBox1.Size = new System.Drawing.Size(278, 21);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.Text = "Seleccione un personaje";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Enter_character
             // 
-            this.Enter_character.Location = new System.Drawing.Point(641, 106);
+            this.Enter_character.Location = new System.Drawing.Point(629, 109);
             this.Enter_character.Name = "Enter_character";
-            this.Enter_character.Size = new System.Drawing.Size(266, 20);
+            this.Enter_character.Size = new System.Drawing.Size(278, 20);
             this.Enter_character.TabIndex = 1;
             this.Enter_character.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.Enter_character.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // search_character
             // 
-            this.search_character.Location = new System.Drawing.Point(268, 584);
+            this.search_character.Location = new System.Drawing.Point(629, 504);
             this.search_character.Name = "search_character";
-            this.search_character.Size = new System.Drawing.Size(266, 20);
+            this.search_character.Size = new System.Drawing.Size(278, 20);
             this.search_character.TabIndex = 2;
             this.search_character.TextChanged += new System.EventHandler(this.serch_character_TextChanged);
             this.search_character.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.serch_character_KeyPress);
@@ -76,7 +82,7 @@
             // 
             // Character_picture
             // 
-            this.Character_picture.Location = new System.Drawing.Point(248, 210);
+            this.Character_picture.Location = new System.Drawing.Point(249, 177);
             this.Character_picture.Name = "Character_picture";
             this.Character_picture.Size = new System.Drawing.Size(304, 326);
             this.Character_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -84,11 +90,47 @@
             this.Character_picture.TabStop = false;
             this.Character_picture.Click += new System.EventHandler(this.Character_picture_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(629, 245);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(278, 177);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(249, 329);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(304, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 40;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 40;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(931, 683);
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(935, 583);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.Character_picture);
             this.Controls.Add(this.characters_names);
             this.Controls.Add(this.search_character);
@@ -110,6 +152,11 @@
         private System.Windows.Forms.TextBox search_character;
         private System.Windows.Forms.ListBox characters_names;
         private System.Windows.Forms.PictureBox Character_picture;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- Zelda's Characters/Form1.cs
+++ Zelda's Characters/Form1.cs
@@ -10,15 +10,22 @@ using System.Windows.Forms;
 using System.IO;
 
 
+
 namespace WindowsFormsApplication1
-{
+{    
     public partial class Form1 : Form
     {
+
+
+
+        int ticks = 0;
+        int ticks1 = 0;
+        int ticks2 = 0;
         public Form1()
         {
             InitializeComponent();
         }
-
+         
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
            
@@ -28,12 +35,7 @@ namespace WindowsFormsApplication1
         {
             if (e.KeyChar == Convert.ToChar(Keys.Enter))
             {
-                comboBox1.Items.Add(Enter_character.Text); 
-                characters_names.Items.Add(Enter_character.Text);
-                Character_picture.Load(@"..\..\Images\" + Enter_character.Text + ".jpg");
-                Enter_character.Text = ("");
-                
-               
+                    timer1.Enabled = true; 
             }
         }
 
@@ -49,7 +51,23 @@ namespace WindowsFormsApplication1
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            timer3.Enabled = true;
+            Character_picture.Visible = false;
+            richTextBox1.Visible = false;
+
             Character_picture.Load(@"..\..\Images\" + comboBox1.Text + ".jpg");
+            StreamReader read_info = new StreamReader(@"..\..\Info\" + comboBox1.Text + ".txt");
+
+            string line;
+
+            line = read_info.ReadLine();
+            while (line != null)
+            {
+                richTextBox1.AppendText(line);
+                line = read_info.ReadLine();
+            }
+            read_info.Close();  
+
         }
 
         private void Character_picture_Click(object sender, EventArgs e)
@@ -66,7 +84,8 @@ namespace WindowsFormsApplication1
         {
             if (e.KeyChar == Convert.ToChar (Keys.Enter))
             {
-                Character_picture.Load(@"..\..\Images\" + search_character.Text + ".jpg");
+                timer2.Enabled = true;
+                
             }
 
 
@@ -83,6 +102,98 @@ namespace WindowsFormsApplication1
                     characters_names.Items.RemoveAt(characters_names.SelectedIndex);
                 }
 
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ("");
+            Character_picture.Visible = false;
+            richTextBox1.Visible = false;
+            ticks++;
+            progressBar1.Visible = true;
+            progressBar1.Value = ticks;
+            if (ticks == 100)
+            {
+                timer1.Stop();
+                progressBar1.Visible = false;
+                ticks = 0;
+                Character_picture.Visible = true;
+                richTextBox1.Visible = true;
+                comboBox1.Items.Add(Enter_character.Text);
+
+                characters_names.Items.Add(Enter_character.Text);
+
+                Character_picture.Load(@"..\..\Images\" + Enter_character.Text + ".jpg");
+
+                StreamReader read_info = new StreamReader(@"..\..\Info\" + Enter_character.Text + ".txt");
+
+                string line;
+
+                line = read_info.ReadLine();
+                while (line != null)
+                {
+                    richTextBox1.AppendText(line);
+                    line = read_info.ReadLine();
+                }
+                read_info.Close();
+                Enter_character.Text = ("");
+
+            }
+
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ("");
+            Character_picture.Visible = false;
+            richTextBox1.Visible = false;
+            progressBar1.Visible = true;
+            ticks1++;
+            progressBar1.Value = ticks1;
+            if (ticks1 == 100)
+            {
+                timer2.Stop();
+                progressBar1.Visible = false;
+                ticks1 = 0;
+                Character_picture.Visible = true;
+                richTextBox1.Visible = true;
+
+                Character_picture.Load(@"..\..\Images\" + search_character.Text + ".jpg");
+
+                StreamReader read_info = new StreamReader(@"..\..\Info\" + search_character.Text + ".txt");
+
+                string line;
+
+                line = read_info.ReadLine();
+                while (line != null)
+                {
+                    richTextBox1.AppendText(line);
+                    line = read_info.ReadLine();
+                }
+                read_info.Close();
+                search_character.Text = ("");
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            ticks2++;
+            progressBar1.Visible = true;
+            progressBar1.Value = ticks2;
+            if (ticks2 == 100)
+            {
+                timer3.Stop();
+                progressBar1.Visible = false;
+                ticks2 = 0;
+                Character_picture.Visible = true;
+                richTextBox1.Visible = true;
             }
         }
     }
