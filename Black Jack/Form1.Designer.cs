f30a61fc23d6bf579619d3ff81a5f5f0d9c0e73e
Diego Ernesto Bahena López <dbahenalopez@hotmail.com>
2019-06-15 18:47:23 -0500

    Problemas con Controles globales

--- Black Jack/Form1.Designer.cs
+++ Black Jack/Form1.Designer.cs
@@ -33,13 +33,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(515, 612);
+            this.button1.Location = new System.Drawing.Point(670, 210);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -57,6 +56,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+
+
+
     }
 }
 
